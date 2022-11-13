c0bd3a6f779956aad0f85398194a3c64c5d127b9
userjohn <johnchacpi479@gmail.com>
2022-11-12 20:19:39 -0500

    attempt to call a fox api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const apiFox = "https://randomfox.ca/floof/";
+const button = document.querySelector("button.p-4");
+const container = document.querySelector("#images");
 
-console.log('Happy hacking :)')
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        const data = response.json();
+        renderImg(data.image)
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+async function renderImg(urlImage) {
+    const div = document.createElement("div");
+    div.className = "p-4";
+    const image = document.createElement("img");
+    image.src = urlImage;
+    image.className = "mx-auto w-32";
+    div.append(image);
+    container.append(div);
+}
+
+async function generateImage() {
+    const img = await fetchData(apiFox);
+}
+button.addEventListener("click", generateImage)
+
