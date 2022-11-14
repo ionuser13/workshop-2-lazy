5678d3fca4dfb01fbb2506195a1aff2455320347
userjohn <johnchacpi479@gmail.com>
2022-11-13 22:10:56 -0500

    used hyperscript to create nodes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import h from "hyperscript"
 import {registerImg} from "./lazy"
 
 const button = document.querySelector("button.p-4");
@@ -8,13 +9,11 @@ const max = 122;
 const random = () => Math.floor(Math.random()*(max - min)) + min
 
 const createImageNode = () => {
-    const div = document.createElement("div");
-    div.className = "my-4";
-    const image = document.createElement("img");
-    image.className = "mx-auto w-60";
-    image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
-    const imgWrap = document.createElement("div");
-    imgWrap.className = "w-60 bg-gray-400 min-h-full p-2";
+    const image = h("img.mx-auto.w-60", {
+        "data-src": `https://randomfox.ca/images/${random()}.jpg`
+    })
+    const div = h("div.my-4.p-4", image)
+    const imgWrap = h("div.w-60.bg-gray-400.min-h-full.p-2")
     imgWrap.style.minHeight = "100px"
     imgWrap.style.display = "inline-block";
     imgWrap.appendChild(image);
